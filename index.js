d8744c8d7429edac185860007398f5acd5a4a8e2
Rizwan Ahmad <113388795+Riz1981@users.noreply.github.com>
2023-10-31 11:46:41 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+//store fav number in variable and show in message
+let favorite_number = 9;
+let message = `my favorite number is ${favorite_number}`;
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,4 @@
+//store fav number in variable and show in message
+let favorite_number:number=9;
+let message:string = `my favorite number is ${favorite_number}`;
+console.log(message);
